8e498078938394ce4f893bb62ac9bf4a5456fc43
BatoulMDarwish <batoulmahmoud687@gmail.com>
2023-06-21 16:20:26 +0300

    first commit

---
+++ lib/core/network/error_model.freezed.dart
@@ -0,0 +1,170 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'error_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+ErrorModel _$ErrorModelFromJson(Map<String, dynamic> json) {
+  return _ErrorModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$ErrorModel {
+  int? get statusCode => throw _privateConstructorUsedError;
+  String? get errorMessage => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ErrorModelCopyWith<ErrorModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ErrorModelCopyWith<$Res> {
+  factory $ErrorModelCopyWith(
+          ErrorModel value, $Res Function(ErrorModel) then) =
+      _$ErrorModelCopyWithImpl<$Res, ErrorModel>;
+  @useResult
+  $Res call({int? statusCode, String? errorMessage});
+}
+
+/// @nodoc
+class _$ErrorModelCopyWithImpl<$Res, $Val extends ErrorModel>
+    implements $ErrorModelCopyWith<$Res> {
+  _$ErrorModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? statusCode = freezed,
+    Object? errorMessage = freezed,
+  }) {
+    return _then(_value.copyWith(
+      statusCode: freezed == statusCode
+          ? _value.statusCode
+          : statusCode // ignore: cast_nullable_to_non_nullable
+              as int?,
+      errorMessage: freezed == errorMessage
+          ? _value.errorMessage
+          : errorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_ErrorModelCopyWith<$Res>
+    implements $ErrorModelCopyWith<$Res> {
+  factory _$$_ErrorModelCopyWith(
+          _$_ErrorModel value, $Res Function(_$_ErrorModel) then) =
+      __$$_ErrorModelCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({int? statusCode, String? errorMessage});
+}
+
+/// @nodoc
+class __$$_ErrorModelCopyWithImpl<$Res>
+    extends _$ErrorModelCopyWithImpl<$Res, _$_ErrorModel>
+    implements _$$_ErrorModelCopyWith<$Res> {
+  __$$_ErrorModelCopyWithImpl(
+      _$_ErrorModel _value, $Res Function(_$_ErrorModel) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? statusCode = freezed,
+    Object? errorMessage = freezed,
+  }) {
+    return _then(_$_ErrorModel(
+      statusCode: freezed == statusCode
+          ? _value.statusCode
+          : statusCode // ignore: cast_nullable_to_non_nullable
+              as int?,
+      errorMessage: freezed == errorMessage
+          ? _value.errorMessage
+          : errorMessage // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_ErrorModel implements _ErrorModel {
+  const _$_ErrorModel({this.statusCode, this.errorMessage});
+
+  factory _$_ErrorModel.fromJson(Map<String, dynamic> json) =>
+      _$$_ErrorModelFromJson(json);
+
+  @override
+  final int? statusCode;
+  @override
+  final String? errorMessage;
+
+  @override
+  String toString() {
+    return 'ErrorModel(statusCode: $statusCode, errorMessage: $errorMessage)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_ErrorModel &&
+            (identical(other.statusCode, statusCode) ||
+                other.statusCode == statusCode) &&
+            (identical(other.errorMessage, errorMessage) ||
+                other.errorMessage == errorMessage));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, statusCode, errorMessage);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_ErrorModelCopyWith<_$_ErrorModel> get copyWith =>
+      __$$_ErrorModelCopyWithImpl<_$_ErrorModel>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_ErrorModelToJson(
+      this,
+    );
+  }
+}
+
+abstract class _ErrorModel implements ErrorModel {
+  const factory _ErrorModel(
+      {final int? statusCode, final String? errorMessage}) = _$_ErrorModel;
+
+  factory _ErrorModel.fromJson(Map<String, dynamic> json) =
+      _$_ErrorModel.fromJson;
+
+  @override
+  int? get statusCode;
+  @override
+  String? get errorMessage;
+  @override
+  @JsonKey(ignore: true)
+  _$$_ErrorModelCopyWith<_$_ErrorModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
